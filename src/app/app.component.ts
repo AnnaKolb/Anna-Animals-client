154e1925af1360ed3d17cae57a2af40f9c0e762f
AnnaKolb <anna.kolb@constantadvancement.com>
2024-07-01 16:24:53 -0400

    final changes + nav try

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 import { ApiServiceService } from './api-service.service';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatTabsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
